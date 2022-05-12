39c7bc29b3193f9a08559b29d56387a9b695623c
RobiKhan <robinson.cung@epfl.ch>
2022-05-12 10:18:11 +0200

    Fix Map 2

---
+++ Assets/FirstPersonView.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonView : MonoBehaviour
+{
+
+    Vector3 position;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //initialPosition = (0,0,0);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
