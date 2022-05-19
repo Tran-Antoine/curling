a0559bacc5049b069596853a727fe2823702ee11
RobiKhan <robinson.cung@epfl.ch>
2022-05-19 11:44:29 +0200

    Implement camera's movement

--- Assets/Scripts/UI/CameraManager.cs
+++ Assets/Scripts/UI/CameraManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 //using System.Windows.Input;
 
-public class FirstPersonView : MonoBehaviour
+public class CameraManager : MonoBehaviour
 {
 
     Vector3 position;
@@ -11,6 +11,7 @@ public class FirstPersonView : MonoBehaviour
     private static float baseAccel = .2f;
     private static float fastAccel = .45f;
 
+    private float rotation_speed = 15f;
 
     private float accel;
     float xmvt;
@@ -27,15 +28,24 @@ public class FirstPersonView : MonoBehaviour
     void Update()
     {
         //Debug.Log(Input.GetKeyDown(KeyCode.L));
+        if(Input.GetMouseButton(0)){
+            transform.eulerAngles += rotation_speed * new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
+        }
         accel = Input.GetKey(KeyCode.LeftShift) ? fastAccel : baseAccel;
         
         xmvt = Input.GetAxis("VerticalWASD") * accel;
-        zmvt = -Input.GetAxis("HorizontalWASD") * accel;
+        zmvt = Input.GetAxis("HorizontalWASD") * accel;
 
-        Vector3 direction = new Vector3(xmvt, 0, zmvt);
+        Vector3 RIGHT = transform.TransformDirection(Vector3.right);
+        Vector3 FORWARD = transform.TransformDirection(Vector3.forward);
+
+        transform.localPosition += RIGHT * zmvt;
+        transform.localPosition += FORWARD * xmvt;
+
+        //Vector3 direction = new Vector3(xmvt, 0, zmvt);
 
 
-        transform.position += direction;
+        //transform.position += direction;
         
     }
 }
